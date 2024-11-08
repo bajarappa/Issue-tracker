92cd300d82cd8e913f5f1c1c90a6676e1910afbf
Kuruba Bajarappa <bajarappa202@gmail.com>
2024-11-08 21:55:19 +0530

    adding build

--- server.js
+++ server.js
@@ -4,11 +4,23 @@ import app from "./index.js";
 // Importing the 'connectToMongoose' function from the 'connectToMongo.js' file
 import { connectToMongoose } from "./src/config/connectToMongo.js";
 
-// Setting up the server to listen on port 3000
-app.listen(3000, () => {
-  // Connecting to the MongoDB database using the imported function
-  connectToMongoose();
+// Define the port, using an environment variable with a fallback to 3000
+const PORT = process.env.PORT || 3000;
 
-  // Logging a message indicating that the app is now listening on port 3000
-  console.log("App is listening at port 3000");
-});
+// Function to start the server
+const startServer = async () => {
+  try {
+    // Connect to MongoDB
+    await connectToMongoose();
+
+    // Start the server only after MongoDB connection is successful
+    app.listen(PORT, () => {
+      console.log(`App is listening at port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+  }
+};
+
+// Start the server
+startServer();
